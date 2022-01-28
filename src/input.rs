3680352edd90dbe1f6e40624d48464eaa563bae9
Lucio Moriconi <c-lucio@beacon.co.uk>
2022-01-28 09:12:17 +0100

    working UI

--- src/input.rs
+++ src/input.rs
@@ -1,18 +1,18 @@
 use super::entities::*;
 use macroquad::prelude::*;
+use macroquad::ui::{hash, root_ui, widgets, Skin};
 
 pub enum Entities {
 	Dot,
 	Polygon,
 }
 
-pub fn draw_mouse_icon(is_creating: bool, creating_entity: &mut Entities) {
-	if !is_creating {
+pub fn draw_mouse_icon(creating_entity: &mut Entities) {
 		match creating_entity {
 			Entities::Dot => {
 				draw_circle_lines(
-					mouse_position().0 + 15.,
-					mouse_position().1 + 15.,
+					mouse_position().0 + 20.,
+					mouse_position().1 + 20.,
 					10.,
 					1.,
 					YELLOW,
@@ -20,17 +20,15 @@ pub fn draw_mouse_icon(is_creating: bool, creating_entity: &mut Entities) {
 			}
 			Entities::Polygon => {
 				draw_rectangle_lines(
-					mouse_position().0 + 5.,
-					mouse_position().1 + 5.,
+					mouse_position().0 + 15.,
+					mouse_position().1 + 15.,
 					20.,
 					20.,
 					2.,
 					YELLOW,
 				);
 			}
-		}
 	}
-	draw_circle(mouse_position().0, mouse_position().1, 2., WHITE);
 }
 
 pub fn spawn_entity(
@@ -42,12 +40,78 @@ pub fn spawn_entity(
 	match entity_type {
 		Entities::Dot => {
 			dots.push(dot::Dot::new(
-				Some(Vec2::new(mouse_position().0, mouse_position().1)),
+				Some(vec2(mouse_position().0, mouse_position().1)),
 				None,
 			));
 		}
 		Entities::Polygon => {
-			drawing_polygon.add_point(Vec2::new(mouse_position().0, mouse_position().1), polygons);
+			drawing_polygon.add_point(vec2(mouse_position().0, mouse_position().1), polygons);
 		}
+	}
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Actions {
+	SetDotCreation,
+	SetPolygonCreation,
+	ResetCanvas,
+	StopDrawing,
+}
+
+pub const BUTTON_SIZE: f32 = 150.;
+pub const BUTTON_OFFSET: f32 = 20.;
+
+pub struct Button {
+	pub action_type: Actions,
+	skin: Skin,
+	order: usize,
+}
+
+impl Button {
+	pub fn new(
+		order: usize,
+		action_type: Actions,
+		background: Vec<u8>,
+		background_hovered: Vec<u8>,
+		background_active: Vec<u8>,
+	) -> Button {
+		let button_style = root_ui()
+			.style_builder()
+			.background(Image::from_file_with_format(&background, None))
+			.background_margin(RectOffset::new(0., BUTTON_SIZE, 67., 67.))
+			.margin(RectOffset::new(0., 0., 0., 0.))
+			.background_hovered(Image::from_file_with_format(&background_hovered, None))
+			.background_clicked(Image::from_file_with_format(&background_active, None))
+			.build();
+		let skin = Skin {
+			button_style,
+			..root_ui().default_skin()
+		};
+		Button { action_type, skin, order }
+	}
+
+	pub fn render_and_action(&self) -> Option<Actions> {
+		let skin = &self.skin;
+		root_ui().push_skin(skin);
+		let mut action: Option<Actions> = None;
+		root_ui().window(
+			hash!(self.order),
+			vec2(
+				BUTTON_OFFSET,
+				BUTTON_OFFSET + ((self.order as f32) * BUTTON_SIZE),
+			),
+			vec2(BUTTON_SIZE, BUTTON_SIZE),
+			|ui| {
+				if widgets::Button::new("").position(vec2(-1., -1.)).ui(ui) {
+					action = Some(self.action_type);
+				}
+			},
+		);
+		root_ui().pop_skin();
+		action
+	}
+
+	pub fn set_dot_creation(creating_entity: &mut Entities) {
+		*creating_entity = Entities::Dot;
 	}
 }
